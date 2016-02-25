335d3f9e46fe7280ea9cb46f8974f07cc889a200
josephsap <joesap1@gmail.com>
2016-02-25 12:50:33 -0700

    tooltip is in

---
+++ general-content-page/index.php
@@ -0,0 +1,25 @@
+<?php 
+	$pageTitle = 'FourPoint Energy';
+	$pageDescription = '';
+	$fbShareImg = '';
+	$thisPage = "";
+	include($_SERVER['DOCUMENT_ROOT']."/includes/header.php"); 
+?>
+
+<header>
+	<h1>The Page Headline</h1>
+	<p>The description goes here.</p>
+</header>
+
+<div class="container">
+	<h2>Sub-header</h2>
+	<p>Man bun yr bushwick asymmetrical, typewriter vice venmo austin. +1 cornhole four loko truffaut, listicle swag street art gochujang disrupt chambray man bun. Master cleanse asymmetrical kale chips meditation sustainable. Shoreditch yr iPhone irony tote bag, forage post-ironic aesthetic occupy pour-over brunch paleo seitan. Hoodie whatever art party, chicharrones kinfolk everyday carry meggings. Etsy fingerstache kitsch, ennui gochujang synth readymade crucifix cliche bicycle rights. Master cleanse waistcoat four dollar toast tattooed kombucha letterpress, art party selvage.</p>
+	<button type="button" class="button btn-white">Read Our History</button>
+	<div class="video">
+		<iframe width="853" height="480" src="https://www.youtube.com/embed/nPzXoiZBkE4?rel=0" frameborder="0" allowfullscreen></iframe>
+	</div>
+	<h3>Another header</h3>
+	<p>occupy pour-over brunch paleo seitan. Hoodie whatever art party, chicharrones kinfolk everyday carry meggings. Etsy fingerstache kitsch, ennui gochujang synth readymade crucifix cliche bicycle rights. Master cleanse waistcoat four dollar toast tattooed kombucha letterpress, art party selvage.</p>
+</div>
+				
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php"); ?>
--- includes/header.php
+++ includes/header.php
@@ -51,6 +51,14 @@
 						</div>
 						<a href="#">Contact</a>
 						<a href="#">Leasing/Selling</a>
+						<a href="#" class="lease-sell">
+							<div class="qmark">
+								<img src="/assets/img/icons/icon-qmark.svg" />
+							</div>
+							<p class="tooltip">
+								typewriter vice venmo austin. +1 cornhole four loko truffaut, listicle swag street art gochujang disrupt chambray man bun. Master cleanse asymmetrical kale chips meditation sustainable. Shoreditch yr iPhone irony tote bag, forage post-ironic aesthetic
+							</p>	
+						</a>
 						<a class="btn" href="#">Login</a>
 					</div>
 					<div id="mobile_menu" class="mobile_nav_icon">MENU&nbsp;&nbsp;<i class="fa fa-bars fa-2"></i></div>
