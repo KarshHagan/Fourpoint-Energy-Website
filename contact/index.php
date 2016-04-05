6e9231df42e5fce4107092f93cadfc277b1a2622
josephsap <joesap1@gmail.com>
2016-04-05 17:26:39 -0600

    styling changes, contact section nth-child

--- contact/index.php
+++ contact/index.php
@@ -108,6 +108,58 @@
 			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24551.60322671729!2d-104.96822526143131!3d39.718297305742986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c7e975627ba33%3A0x30273fae96ab60b3!2s100+St+Paul+St%2C+Denver%2C+CO+80206!5e0!3m2!1sen!2sus!4v1456456498491" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
 		</div>
 	</div>
+	<div class="map-block">
+		<div class="map-address">
+			<div class="inner">
+				<h3>Fourpoint Energy Denver Office</h3>
+				<p>100 St. Paul St., Ste. 300</p>
+				<p>Denver, CO 80206</p>
+				<p><a href="#">303-303-3030</a></p>
+			</div>
+		</div>
+		<div class="google-map">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24551.60322671729!2d-104.96822526143131!3d39.718297305742986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c7e975627ba33%3A0x30273fae96ab60b3!2s100+St+Paul+St%2C+Denver%2C+CO+80206!5e0!3m2!1sen!2sus!4v1456456498491" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</div>
+	</div>
+	<div class="map-block">
+		<div class="map-address">
+			<div class="inner">
+				<h3>Fourpoint Energy Denver Office</h3>
+				<p>100 St. Paul St., Ste. 300</p>
+				<p>Denver, CO 80206</p>
+				<p><a href="#">303-303-3030</a></p>
+			</div>
+		</div>
+		<div class="google-map">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24551.60322671729!2d-104.96822526143131!3d39.718297305742986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c7e975627ba33%3A0x30273fae96ab60b3!2s100+St+Paul+St%2C+Denver%2C+CO+80206!5e0!3m2!1sen!2sus!4v1456456498491" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</div>
+	</div>
+	<div class="map-block">
+		<div class="map-address">
+			<div class="inner">
+				<h3>Fourpoint Energy Denver Office</h3>
+				<p>100 St. Paul St., Ste. 300</p>
+				<p>Denver, CO 80206</p>
+				<p><a href="#">303-303-3030</a></p>
+			</div>
+		</div>
+		<div class="google-map">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24551.60322671729!2d-104.96822526143131!3d39.718297305742986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c7e975627ba33%3A0x30273fae96ab60b3!2s100+St+Paul+St%2C+Denver%2C+CO+80206!5e0!3m2!1sen!2sus!4v1456456498491" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</div>
+	</div>
+	<div class="map-block">
+		<div class="map-address">
+			<div class="inner">
+				<h3>Fourpoint Energy Denver Office</h3>
+				<p>100 St. Paul St., Ste. 300</p>
+				<p>Denver, CO 80206</p>
+				<p><a href="#">303-303-3030</a></p>
+			</div>
+		</div>
+		<div class="google-map">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24551.60322671729!2d-104.96822526143131!3d39.718297305742986!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x876c7e975627ba33%3A0x30273fae96ab60b3!2s100+St+Paul+St%2C+Denver%2C+CO+80206!5e0!3m2!1sen!2sus!4v1456456498491" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</div>
+	</div>
 </section>
 
 
