ab00439700f6978702879ddc4b46716772f74d66
josephsap <joesap1@gmail.com>
2017-06-27 17:00:32 -0600

    county page

--- 2017-business-opps/index.php
+++ 2017-business-opps/index.php
@@ -32,53 +32,113 @@
     </li>
     <li>
       <div class="box-inner">
+        <h3>Aggressive Offers offers stuff offers</h3>
+        <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins:</p>
+      </div>
+    </li>
+    <li>
+      <div class="box-inner">
+        <h3>Aggressive Offers</h3>
+        <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denvethe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesr</p>
+      </div>
+    </li>
+    <li>
+      <div class="box-inner">
         <h3>Aggressive Offers</h3>
         <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decades</p>
+      </div>
+    </li>
+    <li>
+      <div class="box-inner">
+        <h3>Aggressive Offers fffff</h3>
+        <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decades</p>
+      </div>
+    </li>
+    <li>
+      <div class="box-inner">
+        <h3>Aggressive Offers fffff</h3>
+        <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decades</p>
+      </div>
+    </li>
+    <li>
+      <div class="box-inner">
+        <h3>Aggressive Offers fffff</h3>
+        <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe d management team and decades</p>
       </div>
     </li>
   </ol>
   
 
-
-
-
-
-
-
   <ul class="list-reset">
     <li class="accordion-item">
-      <a href="javascript:;" class="accordion-title text-uppercase light-gold-text relative block pb1">People, Culture, &amp; Technology<img src="https://fourpointenergy.com/wp-content/uploads/2017/06/icon-plus2x.png" class="accordion-plus-minus"/></a>
-      <ul class="accordion-content pt1 mt1 relative">
-        <li>Dedicated to creating value guided by our company values and behaviors</li>
-        <li>Support and embody a results-driven culture of opportunistic, performance-driven, and goal-oriented employees and partners</li>
-        <li>Focus and adapt to an always changing environment</li>
-      </ul>
+      <a href="javascript:;" class="accordion-title text-uppercase relative block pb1">People, Culture, &amp; Technology<img src="https://fourpointenergy.com/wp-content/uploads/2017/06/icon-plus2x.png" class="accordion-plus-minus"/></a>
+      <p>The Western Anadarko Basin is located in western Oklahoma and the eastern panhandle of Texas.  This prolific hydrocarbon province contains dense vertical well control and is characterized over twenty gas and oil productive horizons across an area of that covers over 5 million acres.</p>
     </li>
     <li class="accordion-item">
-      <a href="javascript:;" class="accordion-title text-uppercase light-gold-text relative block pb1">Item Two<img src="https://fourpointenergy.com/wp-content/uploads/2017/06/icon-plus2x.png" class="accordion-plus-minus"/></a>
-      <ul class="accordion-content pt1 mt1 relative">
-        <li>Dedicated to creating value guided by our company values and behaviors</li>
-        <li>Support and embody a results-driven culture of opportunistic, performance-driven, and goal-oriented employees and partners</li>
-        <li>Focus and adapt to an always changing environment</li>
-      </ul>
+      <a href="javascript:;" class="accordion-title text-uppercase relative block pb1">Item Two<img src="https://fourpointenergy.com/wp-content/uploads/2017/06/icon-plus2x.png" class="accordion-plus-minus"/></a>
+      <p>The Western Anadarko Basin is located in western Oklahoma and the eastern panhandle of Texas.  This prolific hydrocarbon province contains dense vertical well control and is characterized over twenty gas and oil productive horizons across an area of that covers over 5 million acres.</p>
     </li>
     <li class="accordion-item">
-      <a href="javascript:;" class="accordion-title text-uppercase light-gold-text relative block pb1">ACQUISITIONS, DIVENSTITURES AND JOINT VENTURES<img src="https://fourpointenergy.com/wp-content/uploads/2017/06/icon-plus2x.png" class="accordion-plus-minus"/></a>
-      <ul class="accordion-content pt1 mt1 relative">
-        <li>Dedicated to creating value guided by our company values and behaviors</li>
-        <li>Support and embody a results-driven culture of opportunistic, performance-driven, and goal-oriented employees and partners</li>
-        <li>Focus and adapt to an always changing environment</li>
-      </ul>
-    </li>
-    <li class="accordion-item">
-      <a href="javascript:;" class="accordion-title text-uppercase light-gold-text relative block pb1">Item Four<img src="https://fourpointenergy.com/wp-content/uploads/2017/06/icon-plus2x.png" class="accordion-plus-minus"/></a>
-      <ul class="accordion-content pt1 mt1 relative">
-        <li>Dedicated to creating value guided by our company values and behaviors</li>
-        <li>Support and embody a results-driven culture of opportunistic, performance-driven, and goal-oriented employees and partners</li>
-        <li>Focus and adapt to an always changing environment</li>
-      </ul>
+      <a href="javascript:;" class="accordion-title text-uppercase relative block pb1">ACQUISITIONS, DIVENSTITURES AND JOINT VENTURES<img src="https://fourpointenergy.com/wp-content/uploads/2017/06/icon-plus2x.png" class="accordion-plus-minus"/></a>
+      <p>The Western Anadarko Basin is located in western Oklahoma and the eastern panhandle of Texas.  This prolific hydrocarbon province contains dense vertical well control and is characterized over twenty gas and oil productive horizons across an area of that covers over 5 million acres.</p>
     </li>
   </ul>
+
+
+  <div class="wrapper">
+    <h4>Operating Counties</h4>
+    <div class="pre-footer county-list">
+      
+      <div class="box">
+        <div class="box-inner">
+          <h5>Texas</h5>
+          <ul>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="box">
+        <div class="box-inner">
+          <h5>Oklahoma</h5>
+          <ul class="multicolumn">
+            <li>Hansford</li>
+            <li>Hemphill</li>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+            <li>Hansford</li>
+            <li>Hemphill</li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  </div>
 
   
   </div>
