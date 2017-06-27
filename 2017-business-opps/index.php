7004cfa071f10ba5b61b959a38a7d27c0f66f08d
josephsap <joesap1@gmail.com>
2017-06-27 14:10:42 -0600

    more stuff

---
+++ 2017-business-opps/index.php
@@ -0,0 +1,90 @@
+<?php 
+  $pageTitle = 'FourPoint Energy - Business Strategy';
+  $pageDescription = '';
+  $fbShareImg = '';
+  $thisPage = "";
+  include($_SERVER['DOCUMENT_ROOT']."/includes/header.php"); 
+?>
+
+<header>
+  <h1>Business Opportunities</h1>
+  <p>Introduction copy Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+</header>
+
+<div class="container general-content">
+  <div class="wrapper">
+    <img class="aligncenter size-full wp-image-1142" src="https://fourpointenergy.com/wp-content/uploads/2016/03/strategy.jpg" alt="strategy" width="100%" height="auto" />
+  </div>
+
+
+  <ol class="list-reset counter three-width-box">
+    <li>
+      <div class="box-inner">
+        <h3>Aggressive Offers</h3>
+        <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver</p>
+      </div>
+    </li>
+    <li>
+      <div class="box-inner">
+        <h3>Aggressive Offers</h3>
+        <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decades</p>
+      </div>
+    </li>
+    <li>
+      <div class="box-inner">
+        <h3>Aggressive Offers</h3>
+        <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decades</p>
+      </div>
+    </li>
+  </ol>
+  
+
+
+
+
+
+
+
+  <ul class="list-reset">
+    <li class="accordion-item">
+      <a href="javascript:;" class="accordion-title text-uppercase light-gold-text relative block pb1">People, Culture, &amp; Technology<img src="https://fourpointenergy.com/wp-content/uploads/2017/06/icon-plus2x.png" class="accordion-plus-minus"/></a>
+      <ul class="accordion-content pt1 mt1 relative">
+        <li>Dedicated to creating value guided by our company values and behaviors</li>
+        <li>Support and embody a results-driven culture of opportunistic, performance-driven, and goal-oriented employees and partners</li>
+        <li>Focus and adapt to an always changing environment</li>
+      </ul>
+    </li>
+    <li class="accordion-item">
+      <a href="javascript:;" class="accordion-title text-uppercase light-gold-text relative block pb1">Item Two<img src="https://fourpointenergy.com/wp-content/uploads/2017/06/icon-plus2x.png" class="accordion-plus-minus"/></a>
+      <ul class="accordion-content pt1 mt1 relative">
+        <li>Dedicated to creating value guided by our company values and behaviors</li>
+        <li>Support and embody a results-driven culture of opportunistic, performance-driven, and goal-oriented employees and partners</li>
+        <li>Focus and adapt to an always changing environment</li>
+      </ul>
+    </li>
+    <li class="accordion-item">
+      <a href="javascript:;" class="accordion-title text-uppercase light-gold-text relative block pb1">ACQUISITIONS, DIVENSTITURES AND JOINT VENTURES<img src="https://fourpointenergy.com/wp-content/uploads/2017/06/icon-plus2x.png" class="accordion-plus-minus"/></a>
+      <ul class="accordion-content pt1 mt1 relative">
+        <li>Dedicated to creating value guided by our company values and behaviors</li>
+        <li>Support and embody a results-driven culture of opportunistic, performance-driven, and goal-oriented employees and partners</li>
+        <li>Focus and adapt to an always changing environment</li>
+      </ul>
+    </li>
+    <li class="accordion-item">
+      <a href="javascript:;" class="accordion-title text-uppercase light-gold-text relative block pb1">Item Four<img src="https://fourpointenergy.com/wp-content/uploads/2017/06/icon-plus2x.png" class="accordion-plus-minus"/></a>
+      <ul class="accordion-content pt1 mt1 relative">
+        <li>Dedicated to creating value guided by our company values and behaviors</li>
+        <li>Support and embody a results-driven culture of opportunistic, performance-driven, and goal-oriented employees and partners</li>
+        <li>Focus and adapt to an always changing environment</li>
+      </ul>
+    </li>
+  </ul>
+
+  
+  </div>
+</div>
+
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/pre-footer.php"); ?>
+
+        
+<?php include($_SERVER['DOCUMENT_ROOT']."/includes/footer.php"); ?>
