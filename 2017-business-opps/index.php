5ede315de9bb98afca285ed7a4034e4c6c4d5704
josephsap <joesap1@gmail.com>
2017-06-28 11:26:35 -0600

    county page

--- 2017-business-opps/index.php
+++ 2017-business-opps/index.php
@@ -72,15 +72,15 @@
   <ul class="list-reset">
     <li class="accordion-item">
       <a href="javascript:;" class="accordion-title text-uppercase relative block pb1">People, Culture, &amp; Technology<img src="https://fourpointenergy.com/wp-content/uploads/2017/06/icon-plus2x.png" class="accordion-plus-minus"/></a>
-      <p>The Western Anadarko Basin is located in western Oklahoma and the eastern panhandle of Texas.  This prolific hydrocarbon province contains dense vertical well control and is characterized over twenty gas and oil productive horizons across an area of that covers over 5 million acres.</p>
+      <p class="accordion-content">The Western Anadarko Basin is located in western Oklahoma and the eastern panhandle of Texas.  This prolific hydrocarbon province contains dense vertical well control and is characterized over twenty gas and oil productive horizons across an area of that covers over 5 million acres.</p>
     </li>
     <li class="accordion-item">
       <a href="javascript:;" class="accordion-title text-uppercase relative block pb1">Item Two<img src="https://fourpointenergy.com/wp-content/uploads/2017/06/icon-plus2x.png" class="accordion-plus-minus"/></a>
-      <p>The Western Anadarko Basin is located in western Oklahoma and the eastern panhandle of Texas.  This prolific hydrocarbon province contains dense vertical well control and is characterized over twenty gas and oil productive horizons across an area of that covers over 5 million acres.</p>
+      <p class="accordion-content">The Western Anadarko Basin is located in western Oklahoma and the eastern panhandle of Texas.  This prolific hydrocarbon province contains dense vertical well control and is characterized over twenty gas and oil productive horizons across an area of that covers over 5 million acres.</p>
     </li>
     <li class="accordion-item">
       <a href="javascript:;" class="accordion-title text-uppercase relative block pb1">ACQUISITIONS, DIVENSTITURES AND JOINT VENTURES<img src="https://fourpointenergy.com/wp-content/uploads/2017/06/icon-plus2x.png" class="accordion-plus-minus"/></a>
-      <p>The Western Anadarko Basin is located in western Oklahoma and the eastern panhandle of Texas.  This prolific hydrocarbon province contains dense vertical well control and is characterized over twenty gas and oil productive horizons across an area of that covers over 5 million acres.</p>
+      <p class="accordion-content">The Western Anadarko Basin is located in western Oklahoma and the eastern panhandle of Texas.  This prolific hydrocarbon province contains dense vertical well control and is characterized over twenty gas and oil productive horizons across an area of that covers over 5 million acres.</p>
     </li>
   </ul>
 
@@ -90,10 +90,10 @@
     <div class="pre-footer county-list">
       
       <div class="box">
-        <div class="box-inner">
+        <div class="box-inner clearfix">
           <h5>Texas</h5>
           <ul>
-            <li>Hansford</li>
+            <li>Hansfordfordford</li>
             <li>Hemphill</li>
             <li>Hansford</li>
             <li>Hemphill</li>
@@ -104,12 +104,12 @@
       </div>
 
       <div class="box">
-        <div class="box-inner">
+        <div class="box-inner clearfix">
           <h5>Oklahoma</h5>
-          <ul class="multicolumn">
-            <li>Hansford</li>
-            <li>Hemphill</li>
-            <li>Hansford</li>
+          <ul>
+            <li>Hansfordford</li>
+            <li>Hemphillhillhillhillhill</li>
+            <li>Hansford county</li>
             <li>Hemphill</li>
             <li>Hansford</li>
             <li>Hemphill</li>
