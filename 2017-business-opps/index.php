15b1ad94f57b8c41db3a1a6bd2393575576c79ae
josephsap <joesap1@gmail.com>
2017-06-29 17:27:36 -0600

    %%%

--- 2017-business-opps/index.php
+++ 2017-business-opps/index.php
@@ -44,31 +44,41 @@
     </li>
     <li>
       <div class="box-inner">
-        <h3>Aggressive Offers</h3>
+        <div class="box-top">
+          <h3>Aggressive Offers Aggressive Offers Aggressive OffersAggressive Offers</h3>
+        </div>
         <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denvethe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesr</p>
       </div>
     </li>
     <li>
       <div class="box-inner">
-        <h3>Aggressive Offers</h3>
+        <div class="box-top">
+          <h3>Aggressive Offers Aggressive Offers Aggressive Offer</h3>
+        </div>
         <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decades</p>
       </div>
     </li>
     <li>
       <div class="box-inner">
-        <h3>Aggressive Offers fffff</h3>
+        <div class="box-top">
+          <h3>Aggressive Offers Aggressive Offers Aggressive OffersAggressive Offers</h3>
+        </div>
         <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decades</p>
       </div>
     </li>
     <li>
       <div class="box-inner">
-        <h3>Aggressive Offers fffff</h3>
+        <div class="box-top">
+          <h3>Aggressive Offers Aggressive Offers</h3>
+        </div>
         <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decades</p>
       </div>
     </li>
     <li>
       <div class="box-inner">
-        <h3>Aggressive Offers fffff</h3>
+        <div class="box-top">
+          <h3>Aggressive Offers Aggressive Offers</h3>
+        </div>
         <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe d management team and decades</p>
       </div>
     </li>
--- 2017-ownership/index.php
+++ 2017-ownership/index.php
@@ -18,52 +18,75 @@
   </div>
 
 
-  <ol class="list-reset counter three-width-box">
+  <ol class="list-reset counter three-width-box ownership">
     <li>
       <div class="box-inner">
-        <h3>Aggressive Offers</h3>
-        <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver</p>
+        <div class="box-top">
+          <h3>Aggressive Offers</h3>
+        </div>
+        <ul>
+          <li>Marriage Certificate or Divorce Decree reinstating maiden name or other legal document of name change</li>
+          <li>Marriage Certificate or Divorce Decree reinstating maiden name or other legal document of name change</li>
+          <li>Hansford</li>
+        </ul>
       </div>
     </li>
     <li>
       <div class="box-inner">
-        <h3>Aggressive Offers</h3>
-        <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decades</p>
+        <div class="box-top">
+          <h3>Aggressive Offers</h3>
+        </div>
+        <ul>
+          <li>Marriage Certificate or Divorce Decree reinstating maiden name or</li>
+          <li>Hemphill</li>
+        </ul>
       </div>
     </li>
     <li>
       <div class="box-inner">
-        <h3>Aggressive Offers offers stuff offers</h3>
+        <div class="box-top">
+          <h3>Aggressive Offers Aggressive Offers Aggressive OffersAggressive Offers</h3>
+        </div>
         <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins:</p>
       </div>
     </li>
     <li>
       <div class="box-inner">
-        <h3>Aggressive Offers</h3>
+        <div class="box-top">
+          <h3>Aggressive Offers Aggressive Offers Aggressive OffersAggressive Offers</h3>
+        </div>
         <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denvethe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesr</p>
       </div>
     </li>
     <li>
       <div class="box-inner">
-        <h3>Aggressive Offers</h3>
+        <div class="box-top">
+          <h3>Aggressive Offers Aggressive Offers Aggressive OffersAggressive Offers</h3>
+        </div>
         <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decades</p>
       </div>
     </li>
     <li>
       <div class="box-inner">
-        <h3>Aggressive Offers fffff</h3>
+        <div class="box-top">
+          <h3>Aggressive Offers Aggressive Offers Aggressive OffersAggressive Offers</h3>
+        </div>
         <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decades</p>
       </div>
     </li>
     <li>
       <div class="box-inner">
-        <h3>Aggressive Offers fffff</h3>
+        <div class="box-top">
+          <h3>Aggressive Offers Aggressive Offers</h3>
+        </div>
         <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decades</p>
       </div>
     </li>
     <li>
       <div class="box-inner">
-        <h3>Aggressive Offers fffff</h3>
+        <div class="box-top">
+          <h3>Aggressive Offers Aggressive Offers</h3>
+        </div>
         <p>LongPoint Minerals is a company focused specifically on the purchase of mineral interests in three key US Basins: the Mid-Continent, Permian and Denver-Julesburg. With a highly experienced management team and decadesthe d management team and decades</p>
       </div>
     </li>
